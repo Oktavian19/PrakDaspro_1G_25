e0106f36d57e4efc6b728728e196f43473812c19
Oktavian19 <oktavianeka4@gmail.com>
2023-09-15 14:42:38 +0700

    P3_percobaan3_25

---
+++ src/P3/ContohOperator25.java
@@ -0,0 +1,18 @@
+package P3;
+
+public class ContohOperator25 {
+    public static void main(String[] args) {
+        int x = 10;
+        System.out.println(("x++ = " + x++));
+        System.out.println("Setelah evaluasi, x = " + x);
+        x = 10;
+        System.out.println("++x = " + ++x);
+        System.out.println("Setelah evaluasi, x = " + x);
+        int y = 12;
+        System.out.println(x > y || y == x && y<= x);
+        int z = x ^ y;
+        System.out.println("hasil x ^ y adalah " + z);
+        z %= 2;
+        System.out.println("Hasil akhir " + z);
+    }
+}
