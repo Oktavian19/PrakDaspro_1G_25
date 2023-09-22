7b57588feeb533d8605c294d154b37beb6d816eb
Oktavian19 <oktavianeka4@gmail.com>
2023-09-22 15:40:37 +0700

    P4_percobaan3_pertanyaan2_25

--- src/P4/HargaBayar.java
+++ src/P4/HargaBayar.java
@@ -4,15 +4,26 @@ import java.util.Scanner;
 public class HargaBayar {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
+        Scanner stringsc=new Scanner(System.in);
 
-        int harga, jumlah;
+        int harga, jumlah, jmlhalaman;
         double dis=0.1, total, bayar, jmlDis;
+        String merek;
 
-        System.out.println("Masukkan Harga barang yang dibeli: ");
+        System.out.print("Masukkan Harga barang yang dibeli: ");
         harga=sc.nextInt();
 
-        System.out.println("Masukkan jumlah barang yang dibeli: ");
+        System.out.print("Masukkan jumlah barang yang dibeli: ");
         jumlah=sc.nextInt();
+
+        System.out.print("Masukkan besar diskon yang anda peroleh: ");
+        dis=sc.nextDouble();
+
+        System.out.print("Masukkan nama merek buku: ");
+        merek=stringsc.nextLine();
+
+        System.out.print("Masukkan jumlah halaman buku: ");
+        jmlhalaman=sc.nextInt();
 
         total=harga*jumlah;
 
@@ -22,6 +33,7 @@ public class HargaBayar {
 
         System.out.println("Jumlah diskon yang anda peroleh: " + jmlDis);
         System.out.println("Jumlah yang harus dibayar: " + bayar);
+        System.out.printf("Merek buku yang anda beli adalah %s dengan jumlah halaman %d",merek,jmlhalaman);
 
 
     }
