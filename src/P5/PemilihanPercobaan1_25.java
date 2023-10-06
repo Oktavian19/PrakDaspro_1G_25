80c5974663d82747fa828616371b264169256a76
Oktavian19 <oktavianeka4@gmail.com>
2023-10-06 13:21:43 +0700

    P5_percobaan1_25

---
+++ src/P5/PemilihanPercobaan1_25.java
@@ -0,0 +1,22 @@
+package P5;
+
+import java.util.Scanner;
+
+public class PemilihanPercobaan1_25 {
+    public static void main(String[] args) {
+        
+        Scanner input25 = new Scanner(System.in);
+
+        System.out.print("Masukkan angka: ");
+        int angka= input25.nextInt();
+
+        if (angka % 2 == 0) {
+            System.out.println("Angka "+angka+" bilangan genap");
+        }
+        else{
+            System.out.println("Angka "+angka+" bilangan ganjil");
+        }
+
+        input25.close();
+    }
+}
