840733f5c98d92ef5e1be7c09bce79ce0093da4e
Oktavian19 <oktavianeka4@gmail.com>
2023-10-12 09:01:32 +0700

    perbaikan P6_percobaan2_pertanyaan1_25

--- src/P6/Pemilihan2Percobaan2_25.java
+++ src/P6/Pemilihan2Percobaan2_25.java
@@ -22,6 +22,9 @@ public class Pemilihan2Percobaan2_25 {
             if ((sudut1 == 90) || (sudut2 == 90) || (sudut3 == 90)) {
                 System.out.println("Segitiga tersebut adalah segitiga siku-siku");
             }
+            else if (sudut1 == sudut2 && sudut2 == sudut3){
+                System.out.println("Segitiga tersebut adalah segitiga sama sisi");
+            }
             else if((sudut1 == sudut2) || (sudut1 == sudut3) || (sudut2 == sudut3)){
                 System.out.println("Segitiga tersebut adalah segitiga sama kaki");
             }
