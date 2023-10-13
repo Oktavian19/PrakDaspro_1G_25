72bd115bbadbc5b90ec8a60988ce832971638288
Oktavian19 <oktavianeka4@gmail.com>
2023-10-13 13:48:15 +0700

    P7_percobaan1_25

---
+++ src/P7/ForKelipatan_25.java
@@ -0,0 +1,22 @@
+package P7;
+import java.util.Scanner;
+
+public class ForKelipatan_25 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int kelipatan, jumlah=0, counter=0;
+
+        System.out.print("Masukkan bilangan kelipatan (1-9): ");
+        kelipatan = scan.nextInt();
+
+        for (int i = 1; i <= 50; i++) {
+            if (i % kelipatan == 0) {
+                jumlah += i;
+                counter++;
+            }
+        }
+        System.out.printf("Banyaknya bilangan %d dari 1 sampai 50 adalah %d\n", kelipatan, counter);
+        System.out.printf("Total bilangan kelipatan %d dari 1 samapi 50 adalah %d\n", kelipatan, jumlah);
+    }
+}
