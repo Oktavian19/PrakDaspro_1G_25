0a3e446558e81b7ba3027192d9566513acf3dec9
Oktavian19 <oktavianeka4@gmail.com>
2023-10-27 13:16:32 +0700

    P9_percobaan3_pertanyaan1_25

--- src/P9/ArrayRataNilai25.java
+++ src/P9/ArrayRataNilai25.java
@@ -9,10 +9,14 @@ public class ArrayRataNilai25 {
         int[] nilaiMhs = new int[10];
         double total = 0;
         double rata2;
+        int lulus=0;
 
         for (int i = 0; i < nilaiMhs.length; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-"+(i+1)+" : ");
             nilaiMhs[i] = sc.nextInt();
+            if (nilaiMhs[i]>70) {
+                lulus++;
+            }
         }
 
         for (int i = 0; i < nilaiMhs.length; i++) {
@@ -21,6 +25,7 @@ public class ArrayRataNilai25 {
 
         rata2 = total/nilaiMhs.length;
         System.out.println("Rata-rata nilai = "+rata2);
+        System.out.println("Banyak mahasiswa lulus : "+lulus);
 
         sc.close();
     }
