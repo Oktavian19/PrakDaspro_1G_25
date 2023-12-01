ad47078e398198b0c5cacd91cf71fb810f00afd0
Oktavian Eka Ramadhan <72781555+Oktavian19@users.noreply.github.com>
2023-12-01 13:45:08 +0700

    P13_percobaan4_pertanyaan3_25

--- src/P13/UcapanTerimaKasih_25.java
+++ src/P13/UcapanTerimaKasih_25.java
@@ -1,19 +1,29 @@
 package P13;
-import  java.util.Scanner;
+
+import java.util.Scanner;
 
 public class UcapanTerimaKasih_25 {
     public static String PenerimaUcapan() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Tuliskan NAMA orang yang ingin Anda beri ucapan: ");
         String namaOrang = sc.nextLine();
-        sc.close();
         return namaOrang;
     }
+
     public static void UcapanTerimaKasih() {
+        Scanner sc = new Scanner(System.in);
         String nama = PenerimaUcapan();
-        System.out.println("Thank you "+nama+" for being the best teacher in the world.\n"+
-        "You inspired in me a love for learning and made me feel like I could just ask you anything.");
+        System.out.print("Masukkan ucapan tambahan anda: ");
+        String tambah = sc.nextLine();
+        System.out.println("Thank you " + nama + " for being the best teacher in the world.\n" +
+                "You inspired in me a love for learning and made me feel like I could just ask you anything.");
+        System.out.println(UcapanTambahan(tambah));
     }
+
+    public static String UcapanTambahan(String ucapan) {
+        return ucapan;
+    }
+
     public static void main(String[] args) {
         UcapanTerimaKasih();
     }
