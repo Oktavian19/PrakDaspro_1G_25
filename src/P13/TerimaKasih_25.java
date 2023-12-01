aa98a6e324cb8be01bbb544b53950439e6185349
Oktavian Eka Ramadhan <72781555+Oktavian19@users.noreply.github.com>
2023-12-01 08:54:31 +0700

    P13_percobaan1_25

---
+++ src/P13/TerimaKasih_25.java
@@ -0,0 +1,11 @@
+package P13;
+
+public class TerimaKasih_25 {
+    public static void UcapanTerimaKasih(){
+        System.out.println("Thank you for being teh best teacher in the world.\n"+
+        "You inspired in me a love for learning and made me feel like I could ask you anythiing.");
+    }
+    public static void main(String[] args) {
+        UcapanTerimaKasih();
+    }
+}
